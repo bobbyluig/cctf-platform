c2c203ff2f93dc791b7a13d34bf7d4ed6e13e53c
oskarwirga <oskar@wirga.com>
2015-04-27 21:00:48 -0700

    Comment Account.JS

--- js/account.js
+++ js/account.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 	//Initialize the modal
 	$('.modal-trigger').leanModal();
 	
+	//Send new info to server
 	$('form').submit(function() {
 		form = $(this)
 		$.ajax({
@@ -11,23 +12,28 @@ $(document).ready(function() {
 			url: $(this).attr('action'),
 			data: $(this).serialize(),
 			success: function(data) {
+				//On completion blur and remove active class of the parent label
 				if (data.success == 1) {
 					form.find('input:password').val('').blur().parent().find('i, label').removeClass('active');
 				}
+				//Toast info
 				Materialize.toast(data.message, data.message.length * 150);
 			}
 		});
 		return false;
 	});
-
+	
+	//Logout info to server
 	$('#logout').click(function() {
 		$.ajax({
 			type: 'GET',
 			url: '/api/logout',
 			success: function(data) {
+				//On completion redirect to home page
 				if (data.success == 1) {
 					window.location.href = '/';
 				} else {
+				//On failure display toast with a message
 					Materialize.toast(data.message, data.message.length * 150);
 				}
 			}
