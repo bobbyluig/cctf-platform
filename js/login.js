89c01b05d66dce3490874e34d96eafb16c226c27
oskarwirga <oskar@wirga.com>
2015-04-27 21:07:55 -0700

    Comment Login.JS

--- js/login.js
+++ js/login.js
@@ -3,16 +3,19 @@ $(document).ready(function() {
 	$(".button-collapse").sideNav();
 	//Initialize the modal
 	$('.modal-trigger').leanModal();
-
+	
+	//Get data to confirm login
 	$('form').submit(function() {
 		$.ajax({
 			type: 'POST',
 			url: $(this).attr('action'),
 			data: $(this).serialize(),
 			success: function(data) {
+				//On completion redirect to home page
 				if (data.success == 1) {
 					window.location.href = '/';
 				} else {
+				//On failure display a toast with error message
 					Materialize.toast(data.message, data.message.length * 150);
 				}
 			}
