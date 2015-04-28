267e58bdeed1d4a8049d0407c56cddfd18e0f87d
oskarwirga <oskar@wirga.com>
2015-04-27 17:31:50 -0700

    Commented Other.JS

--- js/other.js
+++ js/other.js
@@ -1,3 +1,4 @@
+//This script allows for dragging out the sideNav menu on mobile (http://materializecss.com/images/menu.gif)
 $('document').ready(function() {
 	$('.button-collapse').sideNav();
 	$('.modal-trigger').leanModal();
